6bc2bd3082c0ead69d6bab296203c557bb78d8f9
Rosano Coutinho <pub@xeari.com>
2021-03-22 07:45:11 -0300

    Build db459bc

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@
 
 			// DATA
 
-			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1616333779889',
+			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1616409890885',
 			_DataPersistenceCacheName: 'OLSKServiceWorkerPersistenceCache',
 			_DataOriginPage: '/write',
 
